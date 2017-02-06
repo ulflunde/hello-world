db50406df9209a5b1e6015e2559973f137aa535d
Ulf Lunde <ulf.lunde@gmail.com>
2017-02-06 10:43:04 +0100

    Update hello-world.php
    object-orientation example

--- hello-world.php
+++ hello-world.php
@@ -1,3 +1,35 @@
 <?php
+class Person
+{
+   public $first;
+   public $last;
+
+   public function __construct($f,$l)
+   {
+       $this->first = $f;
+       $this->last = $l;
+   }
+
+   public function greeting()
+   {
+       return "Hei, jeg heter {$this->first} {$this->last}.";
+   }
+
+   public function staticGreeting($first, $last)
+   {
+       return "Hei, jeg heter $first $last.";
+   }
+}  // class Person
+
+$him = new Person('Ulf','Lunde');
+$her = new Person('Jelena','Lunde');
+
 print "Hello world!";
+echo '<br />';
+
+echo $her->greeting(); 
+echo '<br />';
+
+echo Person::staticGreeting('Ulf','Lunde'); 
+echo '<br />';
 ?>
